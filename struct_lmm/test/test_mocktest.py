4c9c8a83f97e0f88f655167b508f627b905fc6bc
fpcasale <casale@ebi.ac.uk>
2017-05-09 13:20:00 +0100

    removed mocktest

--- struct_lmm/test/test_mocktest.py
+++
@@ -1,9 +0,0 @@
-from numpy.testing import assert_allclose
-from numpy import ones
-
-def test_mocktest():
-    assert_allclose(ones(3), ones(3))
-
-if __name__ == '__main__':
-    __import__('pytest').main([__file__, '-s'])
-
