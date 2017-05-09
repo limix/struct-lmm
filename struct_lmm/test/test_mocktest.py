4a77e0e86a8d72721df5b358b368c36d99ca44c5
fpcasale <casale@ebi.ac.uk>
2017-05-09 11:53:11 +0100

    mocktest fixes

--- struct_lmm/test/test_mocktest.py
+++ struct_lmm/test/test_mocktest.py
@@ -1,7 +1,8 @@
 from numpy.testing import assert_allclose
+from numpy import ones
 
 def test_mocktest():
-    assert_allclose(sp.ones(3), sp.ones(3))
+    assert_allclose(ones(3), ones(3))
 
 if __name__ == '__main__':
     __import__('pytest').main([__file__, '-s'])
