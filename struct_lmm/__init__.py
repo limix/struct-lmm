bea85f434f03649aa7d8ad8d5362f5cf847e8883
fpcasale <casale@ebi.ac.uk>
2017-05-13 02:12:53 +0100

    jupdate inits

--- struct_lmm/__init__.py
+++ struct_lmm/__init__.py
@@ -10,6 +10,7 @@ Write me
 from . import (lmm, runner, utils, interpretation)
 from runner import run_struct_lmm
 from lmm import StructLMM
+from lmm import LMM
 
 __all__ = [
     'lmm', 'runner', 'interpretation', 'utils' 
--- struct_lmm/lmm/__init__.py
+++ struct_lmm/lmm/__init__.py
@@ -1,7 +1,9 @@
 r"""
 - :class:`.StructLMM`
+- :class:`.LMM`
 """
 
 from .structlmm import StructLMM
+from .lmm import LMM
 
-__all__ = ['StructLMM']
+__all__ = ['StructLMM', 'LMM']
