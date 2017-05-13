a56dcb391c562a229e7e56c9cd3445c50f856a24
fpcasale <casale@ebi.ac.uk>
2017-05-13 19:14:28 +0100

    new scripts

---
+++ struct_lmm/scripts/norm_env.py
@@ -0,0 +1,33 @@
+import time
+import sys
+import os
+import numpy as np
+import pandas as pd
+import scipy as sp
+import h5py
+from optparse import OptionParser
+from struct_lmm.utils.sugar_utils import *
+
+def entry_point():
+
+    parser = OptionParser()
+
+    # input files
+    parser.add_option("--in", dest='inf', type=str, default=None)
+    parser.add_option("--out", dest='outf', type=str, default=None)
+    parser.add_option("--no_mean_to_one",
+                      action="store_true",
+                      dest='no_mean_to_one',
+                      default=False)
+    (opt, args) = parser.parse_args()
+
+    assert opt.inf is not None, 'Specify in file!'
+    assert opt.outf is not None, 'Specify out file!'
+
+    E = sp.loadtxt(opt.inf)
+    En = norm_env_matrix(E, no_mean_to_one=opt.no_mean_to_one)
+    sp.savetxt(opt.outf, En)
+
+if __name__=='__main__':
+
+    entry_point()
