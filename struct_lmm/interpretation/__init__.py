f41098f16d426a92021480d1bfeeea650edd968c
Rachel Moore <rm18@mib108982i.internal.sanger.ac.uk>
2018-02-06 16:30:59 +0000

    updated

--- struct_lmm/interpretation/__init__.py
+++ struct_lmm/interpretation/__init__.py
@@ -1,9 +1,11 @@
 r"""
 - :class:`.OptimalRho`
 - :class:`.PredictGenEffect`
+- :class:`.BF`
 """
 
 from .optimal_rho import OptimalRho
 from .predict_genetic_effect import PredictGenEffect
+from .bf import BF
 
-__all__ = ['OptimalRho', 'PredictGenEffect']
+__all__ = ['OptimalRho', 'PredictGenEffect', 'BF']
