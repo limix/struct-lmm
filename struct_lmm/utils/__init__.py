262360030ab5c4dea0c746f632d59358814e82e7
Rachel Moore <rm18@mib108982i.internal.sanger.ac.uk>
2018-10-22 15:16:26 +0100

    deleted old R functionalities

--- struct_lmm/utils/__init__.py
+++ struct_lmm/utils/__init__.py
@@ -3,19 +3,9 @@ r"""
 - :func:`.import_one_pheno_from_csv`
 - :func:`.norm_env_matrix`
 - :func:`.make_out_dir`
-- :class:`.CompQuadFormLiu`
-- :class:`.CompQuadFormDavies`
-- :class:`.CompQuadFormLiuMod`
-- :class:`.CompQuadFormDaviesSkat`
 """
 
 from .fdr import fdr_bh
-from .pvmixchi2 import (
-    CompQuadFormDavies,
-    CompQuadFormDaviesSkat,
-    CompQuadFormLiu,
-    CompQuadFormLiuMod,
-)
 from .sugar_utils import import_one_pheno_from_csv, make_out_dir, norm_env_matrix
 
 __all__ = [
@@ -23,9 +13,5 @@ __all__ = [
     "import_one_pheno_from_csv",
     "norm_env_matrix",
     "make_out_dir",
-    "CompQuadFormLiu",
-    "CompQuadFormDavies",
-    "CompQuadFormLiuMod",
-    "CompQuadFormDaviesSkat",
     "fdr_bh",
 ]
