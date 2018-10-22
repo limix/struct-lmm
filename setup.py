eab6094dd69ac2d6c74962e6498cdd4639d384f1
Danilo Horta <danilo.horta@gmail.com>
2018-10-22 15:51:39 +0100

    one script only

--- setup.py
+++ setup.py
@@ -52,9 +52,6 @@ def setup_package():
     tests_require = ["pytest"]
 
     console_scripts = [
-        "struct_lmm_analyze=struct_lmm.scripts.struct_lmm_analyze:entry_point",
-        "lmm_analyze=struct_lmm.scripts.lmm_analyze:entry_point",
-        "lmm_int_analyze=struct_lmm.scripts.lmm_int_analyze:entry_point",
         "norm_env=struct_lmm.scripts.norm_env:entry_point",
     ]
 
