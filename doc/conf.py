b685182b9ba9b07e7e92c633bd39d7a031f13267
Danilo Horta <danilo.horta@gmail.com>
2019-05-16 20:09:15 +0100

    conf

--- doc/conf.py
+++ doc/conf.py
@@ -1,13 +1,16 @@
-from __future__ import unicode_literals
-
-
 import sphinx_rtd_theme
 
 
-def get_version():
+def _get_version():
     import struct_lmm
 
     return struct_lmm.__version__
+
+
+def _get_name():
+    import struct_lmm
+
+    return struct_lmm.__name__
 
 
 extensions = [
@@ -20,41 +23,21 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
 ]
+
+napoleon_numpy_docstring = True
 templates_path = ["_templates"]
 source_suffix = ".rst"
 master_doc = "index"
-project = "StructLMM"
-copyright = "2017, Rachel Moore, Francesco Paolo Casale, Oliver Stegle"
-author = "Rachel Moore, Francesco Paolo Casale, Oliver Stegle"
-version = get_version()
+project = _get_name()
+copyright = "2017, Danilo Horta, Francesco Paolo Casale, Oliver Stegle, Rachel Moore"
+author = "Danilo Horta, Francesco Paolo Casale, Oliver Stegle, Rachel Moore"
+version = _get_version()
 release = version
 language = None
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "conf.py"]
-pygments_style = "sphinx"
+pygments_style = "default"
 todo_include_todos = False
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-htmlhelp_basename = "struclmmdoc"
-latex_elements = {}
-latex_documents = [
-    (
-        master_doc,
-        "struct_lmm.tex",
-        "StructLMM Documentation",
-        "Rachel Moore, Francesco Paolo Casale, Oliver Stegle",
-        "manual",
-    )
-]
-man_pages = [(master_doc, "struct_lmm", "StructLMM Documentation", [author], 1)]
-texinfo_documents = [
-    (
-        master_doc,
-        "structLMM",
-        "StructLMM Documentation",
-        author,
-        "structLMM",
-        "A mixed-model approach to model complex GxE signals.",
-        "Miscellaneous",
-    )
-]
-intersphinx_mapping = {"https://docs.python.org/": None, "http://matplotlib.org": None}
+htmlhelp_basename = "{}doc".format(project)
+intersphinx_mapping = {"https://docs.python.org/": None}
