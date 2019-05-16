0e984bf45118d8af2735487e330c28d21b7004ac
Danilo Horta <danilo.horta@gmail.com>
2019-05-16 16:57:38 +0100

    first pass

--- struct_lmm/__init__.py
+++ struct_lmm/__init__.py
@@ -4,8 +4,9 @@ StructLMM
 
 """
 from ._lmm import StructLMM
+from ._fdr import fdr_bh
 from ._testit import test
 
 __version__ = "0.3.0"
 
-__all__ = ["StructLMM", "__version__", "test"]
+__all__ = ["StructLMM", "__version__", "test", "fdr_bh"]
